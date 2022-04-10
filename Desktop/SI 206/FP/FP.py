ae166ca908997a27530596483500f04ab0e114c8
Sarah Hurwitz <hurwitzs@umich.edu>
2022-04-09 20:33:55 -0400

    search api

--- Desktop/SI 206/FP/FP.py
+++ Desktop/SI 206/FP/FP.py
@@ -5,18 +5,34 @@ import os
 import sqlite3
 import unittest
 
-def get_data(db_filename):
-    base_url = ""
-    params = {}
-    res = requests.get(base_url, params)
+#def get_data(db_filename):
+    # base_url_apple = "https://itunes.apple.com/search?"
+    # params = {'term': 'jack johnson', 'media' : 'song', 'limit' : 20}
+    # res = requests.get(base_url_apple, params)
+    # data = json.loads(res.text)
+    # return data
+
+    # path = os.path.dirname(os.path.abspath(__file__))
+    # conn = sqlite3.connect(path+'/'+db_filename)
+    # cur = conn.cursor()
+
+
+#def visualize_results(db_filename):
+    # path = os.path.dirname(os.path.abspath(__file__))
+    # conn = sqlite3.connect(path+'/'+db_filename)
+    # cur = conn.cursor()
+
+def call_api(artist):
+    base_url_apple = "https://itunes.apple.com/search?"
+    params = {'term': artist, 'mediaType' : 'song', 'limit' : 20, 'attribute' : 'allArtistTerm'}
+    res = requests.get(base_url_apple, params)
     data = json.loads(res.text)
+    json.dumps(data, indent = 4)
+    song_list = []
+    for dic in data['results']:
+        title = dic['trackName']
+        country = dic['country']
+        song_list.append((title, country))
+    return song_list
 
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_filename)
-    cur = conn.cursor()
-
-
-def visualize_results(db_filename):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_filename)
-    cur = conn.cursor()
+print(call_api('jack johnson'))
