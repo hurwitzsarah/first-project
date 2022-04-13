ebb4ed09a13a8bdd423870184ac8336c6e5f55c9
Sarah Hurwitz <hurwitzs@umich.edu>
2022-04-12 20:59:37 -0400

    beginning

--- Desktop/SI 206/FP/FP.py
+++ Desktop/SI 206/FP/FP.py
@@ -5,34 +5,60 @@ import os
 import sqlite3
 import unittest
 
-#def get_data(db_filename):
-    # base_url_apple = "https://itunes.apple.com/search?"
-    # params = {'term': 'jack johnson', 'media' : 'song', 'limit' : 20}
-    # res = requests.get(base_url_apple, params)
-    # data = json.loads(res.text)
-    # return data
 
-    # path = os.path.dirname(os.path.abspath(__file__))
-    # conn = sqlite3.connect(path+'/'+db_filename)
-    # cur = conn.cursor()
+def read_cache(filename):
+    try:
+        file = open(filename, 'r')
+        data = json.loads(file.read())
+        file.close()
+        return data
+    except:
+        return {}
 
+def write_cache(filename, cache_dic):
+    written_cache_file = open(filename, 'w')
+    json_cache_dict = json.dumps(cache_dic)
+    written_cache_file.write(json_cache_dict)
+    written_cache_file.close()
 
-#def visualize_results(db_filename):
-    # path = os.path.dirname(os.path.abspath(__file__))
-    # conn = sqlite3.connect(path+'/'+db_filename)
-    # cur = conn.cursor()
-
-def call_api(artist):
-    base_url_apple = "https://itunes.apple.com/search?"
-    params = {'term': artist, 'mediaType' : 'song', 'limit' : 20, 'attribute' : 'allArtistTerm'}
-    res = requests.get(base_url_apple, params)
+def get_ids(query):
+    search_url = "https://collectionapi.metmuseum.org/public/collection/v1/search"
+    p = {"q" : query}
+    res = requests.get(search_url, params = p)
     data = json.loads(res.text)
-    json.dumps(data, indent = 4)
-    song_list = []
-    for dic in data['results']:
-        title = dic['trackName']
-        country = dic['country']
-        song_list.append((title, country))
-    return song_list
+    #print(json.dumps(data, indent = 4))
+    object_ids = data['objectIDs']
+    return object_ids
 
-print(call_api('jack johnson'))
+def get_object_info(query):
+    object_data_for_cache = []
+    object_ids = get_ids(query)
+    for id in object_ids:
+        object_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(id)
+        res = requests.get(object_url)
+        data = json.loads(res.text)
+        object_data_for_cache.append(data)
+    
+
+#print(get_ids("disease"))
+get_object_info("disease")
+
+# def get_data_with_caching():
+#     cache_dic = read_cache(CACHE_FNAME)
+#     if request_url in cache_dic:
+#         print(f"Using cache for {term}")
+#         return cache_dic[request_url]
+#     else:
+#         print(f"Fetching data for {term}")
+#         try:
+#             res = requests.get(request_url) 
+#             data = json.loads(res.text)
+#         except:
+#             return "Exception"
+#         if data['resultCount'] != 1:
+#             print("Request not set correctly")
+#             return None
+#         else:
+#             cache_dic[request_url] = data['results'][0]
+#             write_cache(CACHE_FNAME, cache_dic) 
+#             return data['results'][0]
