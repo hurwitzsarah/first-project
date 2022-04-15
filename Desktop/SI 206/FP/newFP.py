c45501adbb8d692817a3fb6785fa3dc9380e6c86
Sarah Hurwitz <hurwitzs@umich.edu>
2022-04-15 11:49:11 -0400

    count

--- Desktop/SI 206/FP/newFP.py
+++ Desktop/SI 206/FP/newFP.py
@@ -18,7 +18,7 @@ def get_ids(cur, conn, query):
     return object_ids
 
 def add_to_database(cur, conn, query, db_filename, start, end):
-    cur.execute("CREATE TABLE IF NOT EXISTS met_objects (object_id INTEGER PRIMARY KEY, is_highlight TEXT, title TEXT, artist_name TEXT, artist_enddate INTEGER, medium TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS met_objects (object_id INTEGER PRIMARY KEY, is_highlight TEXT, title TEXT, artist_name TEXT, object_enddate INTEGER, objectname TEXT, medium TEXT)")
     conn.commit()
     object_ids = get_ids(cur, conn, query)
     for id in object_ids[start:end]:
@@ -36,22 +36,33 @@ def add_to_database(cur, conn, query, db_filename, start, end):
                 is_highlight = data.get("isHighlight", 0)
                 title = data.get("title", 0)
                 artist_name = data.get("artistDisplayName", 0)
-                artist_enddate = data.get("artistEndDate", 0)
+                object_enddate = data.get("objectEndDate", 0)
+                objectname = data.get("objectName", 0)
                 medium = data.get("medium", 0)
-                cur.execute('''INSERT or ignore INTO met_objects (object_id, title, artist_name, artist_enddate, medium, is_highlight) 
-                VALUES (?,?,?,?,?,?)''',(id, title, artist_name, artist_enddate, medium, is_highlight,))
+                cur.execute('''INSERT or ignore INTO met_objects (object_id, title, artist_name, object_enddate, objectname, medium, is_highlight) 
+                VALUES (?,?,?,?,?,?,?)''',(id, title, artist_name, object_enddate, objectname, medium, is_highlight))
                 conn.commit()
         
+def join_tables(cur, conn): #returns list of objects that were made after 1900 and are highlighted
+    cur.execute('''SELECT met_objects.object_id, object_ids.met_id, met_objects.title, met_objects.artist_name, met_objects.object_enddate, met_objects.objectname, met_objects.medium, met_objects.is_highlight FROM met_objects JOIN object_ids 
+    ON met_objects.object_id = object_ids.id WHERE met_objects.is_highlight = ? AND met_objects.object_enddate > ?''', (1, 1900))
+    conn.commit()
+    lst = cur.fetchall()
+    count = 0
+    for item in lst:
+        count += 1
+    return count
 
 def main():
-    db_filename = 'art.db'
+    db_filename = 'art_pieces.db'
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_filename)
     cur = conn.cursor()
-    add_to_database(cur, conn, "disease",'art.db', 0, 25)
-    add_to_database(cur, conn, "disease",'art.db', 25, 50)
-    add_to_database(cur, conn, "disease",'art.db', 50, 75)
-    add_to_database(cur, conn, "disease",'art.db', 75, 100)
+    add_to_database(cur, conn, "disease",'art_pieces.db', 0, 25)
+    add_to_database(cur, conn, "disease",'art_pieces.db', 25, 50)
+    add_to_database(cur, conn, "disease",'art_pieces.db', 50, 75)
+    add_to_database(cur, conn, "disease",'art_pieces.db', 75, 100)
+    join_tables(cur, conn)
 
 if __name__ == "__main__":
     main()
