80455a0c99d44e60744bf8f5b336a55efcbf3543
Sarah Hurwitz <hurwitzs@umich.edu>
2022-04-15 11:05:15 -0400

    second table

--- Desktop/SI 206/FP/newFP.py
+++ Desktop/SI 206/FP/newFP.py
@@ -5,19 +5,22 @@ import os
 import sqlite3
 import unittest
 
-def get_ids(query):
+def get_ids(cur, conn, query):
     search_url = "https://collectionapi.metmuseum.org/public/collection/v1/search"
     p = {"q" : query}
     res = requests.get(search_url, params = p)
     data = json.loads(res.text)
     object_ids = data['objectIDs']
+    cur.execute("CREATE TABLE IF NOT EXISTS object_ids (id INTEGER PRIMARY KEY, met_id INTEGER)")
+    for i in range(len(object_ids)):
+        cur.execute("INSERT OR IGNORE INTO object_ids (id,met_id) VALUES (?,?)",(i,object_ids[i]))
+    conn.commit()
     return object_ids
-
 
 def add_to_database(cur, conn, query, db_filename, start, end):
     cur.execute("CREATE TABLE IF NOT EXISTS met_objects (object_id INTEGER PRIMARY KEY, is_highlight TEXT, title TEXT, artist_name TEXT, artist_enddate INTEGER, medium TEXT)")
     conn.commit()
-    object_ids = get_ids(query)
+    object_ids = get_ids(cur, conn, query)
     for id in object_ids[start:end]:
         try:
             object_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(id)
@@ -28,18 +31,20 @@ def add_to_database(cur, conn, query, db_filename, start, end):
         for k, v in data.items():
             if v != '':
                 object_id = int(data.get("objectID", 0))
+                cur.execute("SELECT id FROM object_ids WHERE met_id = ?", (object_id,))
+                id = int(cur.fetchone()[0])
                 is_highlight = data.get("isHighlight", 0)
                 title = data.get("title", 0)
                 artist_name = data.get("artistDisplayName", 0)
                 artist_enddate = data.get("artistEndDate", 0)
                 medium = data.get("medium", 0)
                 cur.execute('''INSERT or ignore INTO met_objects (object_id, title, artist_name, artist_enddate, medium, is_highlight) 
-                VALUES (?,?,?,?,?,?)''',(object_id, title, artist_name, artist_enddate, medium, is_highlight,))
+                VALUES (?,?,?,?,?,?)''',(id, title, artist_name, artist_enddate, medium, is_highlight,))
                 conn.commit()
         
 
 def main():
-    db_filename = 'met.db'
+    db_filename = 'art.db'
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_filename)
     cur = conn.cursor()
